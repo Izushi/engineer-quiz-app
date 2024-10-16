c1af9ff2f810ab530e22434c64254148f815f287
Izushi <shoeshinesamurai@gmail.com>
2024-10-16 15:38:53 -0700

    add relations to each Models

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function options()
+    {
+        return $this->hasMany(Option::class);
+    }
 }
